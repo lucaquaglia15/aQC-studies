e60fa7897e7fcd0c9f14e9f28196ae9800b4a791
Luca Quaglia <luca.quaglia@cern.ch>
2025-02-25 13:38:35 +0100

    Added code to delete the flag .txt output files every time the script is launched

--- script_macro_data_MC_comparison_runlist.py
+++ script_macro_data_MC_comparison_runlist.py
@@ -5,8 +5,8 @@ import os
 # Function that execute the ROOT macro
 def execution_macro_comparison(run):
     
-    macro_dir = "/home/sarapc/Desktop/MID_QC/ComparisonDataMC_A02D"  # path of the folder that contains Data_MC_comparison_run.C
-    comando = ["root", "-l", "-q", f'{macro_dir}/Data_MC_comparison_run.C+("LHC24g3","PbPb2023","apass4",{run})']
+    macro_dir = "/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies"  # path of the folder that contains Data_MC_comparison_run.C
+    comando = ["root", "-l", "-q", f'{macro_dir}/Data_MC_comparison_run.C+("LHC24j4","pp900GeV","apass1",{run})']
 
     result = subprocess.run(comando, capture_output=True, text=True)
 
@@ -14,8 +14,41 @@ def execution_macro_comparison(run):
     print("STDERR:", result.stderr)
 
 # Read the run list from common_run_list.txt
-percorso_file = os.path.join(os.getcwd(), '/home/sarapc/Desktop/MID_QC/ComparisonDataMC_A02D/PbPb2023_apass4/common_run_list.txt') #directory of the period
+percorso_file = os.path.join(os.getcwd(), '/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies/pp900GeV_apass1/common_run_list.txt') #directory of the period
 
+#Remove output files if they exist in the folder
+baseFodlder = "/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies/pp900GeV_apass1/"
+try:
+    os.remove(baseFodlder+"goodRuns.txt")
+except OSError:
+    pass
+
+try:
+    os.remove(baseFodlder+"goodRuns_2sigmas.txt")
+except OSError:
+    pass
+
+try:
+    os.remove(baseFodlder+"goodRuns_chi2.txt")
+except OSError:
+    pass
+
+try:
+    os.remove(baseFodlder+"badRuns.txt")
+except OSError:
+    pass
+
+try:
+    os.remove(baseFodlder+"badRuns_2sigmas.txt")
+except OSError:
+    pass
+
+try:
+    os.remove(baseFodlder+"badRuns_chi2.txt")
+except OSError:
+    pass
+
+# Process all runs in common run list
 with open(percorso_file, 'r') as file:
     for line in file:
         run = line.strip()  # remove any extra spaces or characters
