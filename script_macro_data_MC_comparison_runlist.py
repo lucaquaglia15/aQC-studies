a88b2e9917249a7048abd7bb9ba0bee750a3aab8
Sara Pucillo <sara.pucillo@cern.ch>
2025-03-17 19:41:07 +0100

    Adding to the macros thresholds+sigmas method for quality flag

--- script_macro_data_MC_comparison_runlist.py
+++ script_macro_data_MC_comparison_runlist.py
@@ -5,8 +5,8 @@ import os
 # Function that execute the ROOT macro
 def execution_macro_comparison(run):
     
-    macro_dir = "/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies"  # path of the folder that contains Data_MC_comparison_run.C
-    comando = ["root", "-l", "-q", f'{macro_dir}/Data_MC_comparison_run.C+("LHC24j4","pp900GeV","apass1",{run})']
+    macro_dir = "/home/sarapc/Desktop/MID_QC/ComparisonDataMC_A02D"  # path of the folder that contains Data_MC_comparison_run.C
+    comando = ["root", "-l", "-q", f'{macro_dir}/Data_MC_comparison_run.C+("LHC23k4g","pp2023","apass4",{run})']
 
     result = subprocess.run(comando, capture_output=True, text=True)
 
@@ -14,10 +14,9 @@ def execution_macro_comparison(run):
     print("STDERR:", result.stderr)
 
 # Read the run list from common_run_list.txt
-percorso_file = os.path.join(os.getcwd(), '/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies/pp900GeV_apass1/common_run_list.txt') #directory of the period
+percorso_file = os.path.join(os.getcwd(), '/home/sarapc/Desktop/MID_QC/ComparisonDataMC_A02D/pp136TeV2023_apass4/common_run_list.txt') #directory of the period
 
-#Remove output files if they exist in the folder
-baseFodlder = "/home/luca/cernbox/assegnoTorino/MIDQC/aQC-studies/pp900GeV_apass1/"
+baseFodlder = "/home/sarapc/Desktop/MID_QC/ComparisonDataMC_A02D/pp136TeV2023_apass4"
 try:
     os.remove(baseFodlder+"goodRuns.txt")
 except OSError:
@@ -30,6 +29,11 @@ except OSError:
 
 try:
     os.remove(baseFodlder+"goodRuns_chi2.txt")
+except OSError:
+    pass
+    
+try:
+    os.remove(baseFodlder+"goodRuns_thresholds_sigmas.txt")
 except OSError:
     pass
 
@@ -47,8 +51,11 @@ try:
     os.remove(baseFodlder+"badRuns_chi2.txt")
 except OSError:
     pass
-
-# Process all runs in common run list
+    
+try:
+    os.remove(baseFodlder+"badRuns_thresholds_sigmas.txt")
+except OSError:
+    pass
 with open(percorso_file, 'r') as file:
     for line in file:
         run = line.strip()  # remove any extra spaces or characters
